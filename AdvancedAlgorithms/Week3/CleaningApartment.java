5c4f75434510c55ff86e651cfc9b5198bf10a0ae
adhikari18 <ashok.adhikari18@gmail.com>
2019-02-07 21:03:23 -0500

    Update CleaningApartment.java

--- AdvancedAlgorithms/Week3/CleaningApartment.java
+++ AdvancedAlgorithms/Week3/CleaningApartment.java
@@ -1,168 +1,188 @@
-//time exceeded
 import java.io.*;
-import java.util.*;
+import java.util.Locale;
+import java.util.StringTokenizer;
 
 public class CleaningApartment {
- private final InputReader reader;
- private final OutputWriter writer;
+    private final InputReader reader;
+    private final OutputWriter writer;
 
- public CleaningApartment(InputReader reader, OutputWriter writer) {
-  this.reader = reader;
-  this.writer = writer;
- }
-
- public static void main(String[] args) {
-  InputReader reader = new InputReader(System.in);
-  OutputWriter writer = new OutputWriter(System.out);
-  new CleaningApartment(reader, writer).run();
-  writer.writer.flush();
- }
-
- class Edge {
-  int from;
-  int to;
- }
-
- class ConvertHampathToSat {
-  int numVertices;
-  Edge[] edges;
-
-  ConvertHampathToSat(int n, int m) {
-   numVertices = n;
-   edges = new Edge[m];
-   for (int i = 0; i < m; ++i) {
-    edges[i] = new Edge();
-   }
-  }
-
-  void addExactlyOnce(LinkedList<LinkedList> clauses){
-   for(int i=1; i<=numVertices; i++){
-    LinkedList<Integer> clause = new LinkedList<Integer>();
-    for(int j=0; j<numVertices; j++) clause.add(i+j*numVertices);
-    clauses.add(clause);
-    for(int j=0; j<numVertices-1; j++){
-     for(int k=j+1; k<numVertices; k++){
-      clause = new LinkedList<Integer>();
-      clause.add(-i-j*numVertices);
-      clause.add(-i-k*numVertices);
-      clauses.add(clause);
-     }
+    public CleaningApartment(InputReader reader, OutputWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
     }
-   }
-  }
 
-  void printEquisatisfiableSatFormula() {
-   LinkedList<LinkedList> clauses = new LinkedList<LinkedList>();
-   boolean[][] adj = new boolean[numVertices][numVertices];
-   formAdjMatrix(adj);
-   addExactlyOnce(clauses);
-   addConstraints(clauses, adj);
-   oneVertexEachPosition(clauses);
-   writer.printf("%d %d\n", clauses.size(), numVertices*numVertices);
-   for(LinkedList<Integer> clause: clauses){
-    for(int v: clause) writer.printf("%d ",v);
-    writer.printf("0\n");
-   }
-  }
-  
-  void oneVertexEachPosition(LinkedList<LinkedList> clauses){
-   for(int k=0;k<numVertices;k++){
-    LinkedList<Integer> clause = new LinkedList<Integer>();
-    for(int i=1; i<=numVertices; i++) clause.add(i+k*numVertices);
-    clauses.add(clause);
-    for(int i=1;i<=numVertices-1;i++){
-     for(int j=i+1;j<=numVertices; j++){
-      clause = new LinkedList<Integer>();
-      clause.add(-i-k*numVertices);
-      clause.add(-j-k*numVertices);
-      clauses.add(clause);
-     }
+    public static void main(String[] args) {
+        InputReader reader = new InputReader(System.in);
+        OutputWriter writer = new OutputWriter(System.out);
+        new CleaningApartment(reader, writer).run();
+        writer.writer.flush();
     }
-   }
-  }
-  
-  void formAdjMatrix(boolean[][] adj){
-   for(Edge edge: edges){
-    adj[edge.from-1][edge.to-1] = true;
-    adj[edge.to-1][edge.from-1] = true;
-   }
-  }
 
-  void addConstraints(LinkedList<LinkedList> clauses, boolean[][] adj){
-   for(int i=1; i<=numVertices-1; i++){
-    for(int j=i; j<=numVertices; j++){
-     if(!adj[i-1][j-1] && i!=j){
-      for(int k=0; k<numVertices-1; k++){
-       LinkedList<Integer> clause = new LinkedList<Integer>();
-       clause.add(-i-k*numVertices);
-       clause.add(-j-(k+1)*numVertices);
-       clauses.add(clause);
-       clause = new LinkedList<Integer>();
-       clause.add(-j-k*numVertices);
-       clause.add(-i-(k+1)*numVertices);
-       clauses.add(clause);
-      }
-     }
+    class Edge {
+        int from;
+        int to;
     }
-   }
-  }
- }
 
- public void run() {
-  int n = reader.nextInt();
-  int m = reader.nextInt();
+    class ConvertHampathToSat {
+        int numVertices;
+        Edge[] edges;
+        int countClauses;
 
-  ConvertHampathToSat converter = new ConvertHampathToSat(n, m);
-  for (int i = 0; i < m; ++i) {
-   converter.edges[i].from = reader.nextInt();
-   converter.edges[i].to = reader.nextInt();
-  }
+        ConvertHampathToSat(int n, int m) {
+            numVertices = n;
+            edges = new Edge[m];
+            for (int i = 0; i < m; ++i) {
+                edges[i] = new Edge();
+            }
+        }
 
-  converter.printEquisatisfiableSatFormula();
- }
+        void printEquisatisfiableSatFormula() {
+            StringBuilder clauses = new StringBuilder();
 
- static class InputReader {
-  public BufferedReader reader;
-  public StringTokenizer tokenizer;
+            appearInPath(clauses);
+            allVertexInPath(clauses);
+            onlyOnceInPath(clauses);
+            onlyOneInEachPosition(clauses);
+            connectedVertex(clauses);
+            writer.printf(countClauses + " " + numVertices * numVertices + "\n");
+            writer.printf(clauses.toString());
+        }
 
-  public InputReader(InputStream stream) {
-   reader = new BufferedReader(new InputStreamReader(stream), 32768);
-   tokenizer = null;
-  }
+        private void appearInPath(StringBuilder clauses) {
+            for (int i = 1; i < numVertices * numVertices + 1; i += numVertices) {
+                for (int j = 0; j < numVertices; j++) {
+                    clauses.append(i + j)
+                            .append(" ");
+                }
+                clauses.append("0\n");
+                countClauses++;
+            }
+        }
 
-  public String next() {
-   while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-    try {
-     tokenizer = new StringTokenizer(reader.readLine());
-    } catch (IOException e) {
-     throw new RuntimeException(e);
+        private void allVertexInPath(StringBuilder clauses) {
+            for (int i = 1; i < numVertices + 1; i++) {
+                for (int j = 0; j < numVertices * numVertices; j += numVertices) {
+                    clauses.append(i + j)
+                            .append(" ");
+                }
+                clauses.append("0\n");
+                countClauses++;
+            }
+        }
+
+        private void onlyOnceInPath(StringBuilder clauses) {
+            for (int i = 1; i < numVertices * numVertices + 1; i += numVertices) {
+                for (int j = 0; j < numVertices; j++) {
+                    for (int k = j + 1; k < numVertices; k++) {
+                        clauses.append(-(i + j))
+                                .append(" ")
+                                .append(-(i + k))
+                                .append(" 0\n");
+                        countClauses++;
+                    }
+                }
+
+            }
+        }
+
+        private void onlyOneInEachPosition(StringBuilder clauses) {
+            for (int i = 1; i < numVertices + 1; i++) {
+                for (int j = 0; j < numVertices * numVertices; j += numVertices) {
+                    for (int k = j + numVertices; k < numVertices * numVertices; k += numVertices) {
+                        clauses.append(-(i + j))
+                                .append(" ")
+                                .append(-(i + k))
+                                .append(" 0\n");
+                        countClauses++;
+                    }
+                }
+
+            }
+        }
+
+        private void connectedVertex(StringBuilder clauses) {
+            boolean[][] adj = new boolean[numVertices][numVertices];
+            for (Edge edge : edges) {
+                adj[edge.from - 1][edge.to - 1] = true;
+                adj[edge.to - 1][edge.from - 1] = true;
+            }
+            for (int i = 0; i < numVertices; i++) {
+                for (int j = i + 1; j < numVertices; j++) {
+                    if (!adj[i][j]) {
+                        for (int k = 0; k < numVertices - 1; k++) {
+                            clauses.append(-((i + 1) * numVertices - (numVertices - 1) + k))
+                                    .append(" ")
+                                    .append(-((j + 1) * numVertices - (numVertices - 1) + k + 1))
+                                    .append(" 0\n")
+
+                                    .append(-((j + 1) * numVertices - (numVertices - 1) + k))
+                                    .append(" ")
+                                    .append(-((i + 1) * numVertices - (numVertices - 1) + k + 1))
+                                    .append(" 0\n");
+                            countClauses += 2;
+                        }
+                    }
+                }
+            }
+        }
+
     }
-   }
-   return tokenizer.nextToken();
-  }
 
-  public int nextInt() {
-   return Integer.parseInt(next());
-  }
+    public void run() {
+        int n = reader.nextInt();
+        int m = reader.nextInt();
 
-  public double nextDouble() {
-   return Double.parseDouble(next());
-  }
+        ConvertHampathToSat converter = new ConvertHampathToSat(n, m);
+        for (int i = 0; i < m; ++i) {
+            converter.edges[i].from = reader.nextInt();
+            converter.edges[i].to = reader.nextInt();
+        }
 
-  public long nextLong() {
-   return Long.parseLong(next());
-  }
- }
+        converter.printEquisatisfiableSatFormula();
+    }
 
- static class OutputWriter {
-  public PrintWriter writer;
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
 
-  OutputWriter(OutputStream stream) {
-   writer = new PrintWriter(stream);
-  }
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
 
-  public void printf(String format, Object... args) {
-   writer.print(String.format(Locale.ENGLISH, format, args));
-  }
- }
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    static class OutputWriter {
+        public PrintWriter writer;
+
+        OutputWriter(OutputStream stream) {
+            writer = new PrintWriter(stream);
+        }
+
+        public void printf(String format, Object... args) {
+            writer.print(String.format(Locale.ENGLISH, format, args));
+        }
+    }
 }
